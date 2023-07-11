59a4d7f644837856f63b422163a239be0de3bdf1
PamRios <124759381+PamRios@users.noreply.github.com>
2023-07-11 04:08:52 +0000

    ⭐Best Practice 2: Nombre de los recursos en plural

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const v1Router = require("./v1/routes");
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,8 +8,7 @@ app.get("/", (req, res) => {
     res.send("<h2> It's Working! </h2>");
 });
 
-
-app.use("/api/v1", v1Router);
+app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
     console.log(`API is listening on port ${PORT}`);
